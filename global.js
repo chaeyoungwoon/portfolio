623f93d8a6d1886667571e62e60c062a460a833e
Ethan Choi <147696027+chaeyoungwoon@users.noreply.github.com>
2026-04-16 16:54:38 -0700

    global.js + light/dark mode

---
+++ global.js
@@ -0,0 +1,72 @@
+console.log("IT'S ALIVE!");
+
+// Helper for selecting elements
+export function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+}
+
+// --- STEP 3: AUTOMATIC NAVIGATION ---
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/chaeyoungwoon', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') ? '/' : '/portfolio/';
+
+for (let p of pages) {
+    let url = p.url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = p.title;
+
+    // Highlight current page and handle external links
+    a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+    if (a.host !== location.host) a.target = "_blank";
+
+    nav.append(a);
+}
+
+// --- STEP 4: DARK MODE SWITCHER ---
+document.body.insertAdjacentHTML('afterbegin', `
+    <label class="color-scheme">
+        Theme:
+        <select id="theme-select">
+            <option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>`);
+
+const select = document.querySelector('#theme-select');
+
+function setColorScheme(color) {
+    document.documentElement.style.setProperty('color-scheme', color);
+    select.value = color;
+    localStorage.colorScheme = color; // Save preference
+}
+
+if ("colorScheme" in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', (e) => setColorScheme(e.target.value));
+
+// --- STEP 5: BETTER CONTACT FORM ---
+let form = document.querySelector('form');
+form?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = new FormData(form);
+    let url = form.action + "?";
+    for (let [name, value] of data) {
+        url += `${name}=${encodeURIComponent(value)}&`;
+    }
+    location.href = url;
+});
