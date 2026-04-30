e89f94c00ef04a894da797bae961e508eccd31b6
Ethan Choi <147696027+chaeyoungwoon@users.noreply.github.com>
2026-04-30 14:22:29 -0700

    update lab 5

--- projects/projects.js
+++ projects/projects.js
@@ -1,23 +1,23 @@
 import { fetchJSON, renderProjects } from '../global.js';
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm'; [cite: 663]
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 const searchInput = document.querySelector('.searchBar');
 
-let query = ''; [cite: 1060]
-let selectedIndex = -1; [cite: 1297]
-let colors = d3.scaleOrdinal(d3.schemeTableau10); [cite: 824]
+let query = '';
+let selectedIndex = -1;
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 function filterAndRender() {
     let filtered = projects.filter((project) => {
-        let values = Object.values(project).join('\n').toLowerCase(); [cite: 1128]
-        return values.includes(query.toLowerCase()); [cite: 1129]
+        let values = Object.values(project).join('\n').toLowerCase();
+        return values.includes(query.toLowerCase());
     });
 
-    renderPieChart(filtered); [cite: 1164]
+    renderPieChart(filtered);
 
-    if (selectedIndex !== -1) { [cite: 1367]
+    if (selectedIndex !== -1) {
         let rolledData = d3.rollups(filtered, v => v.length, d => d.year);
         if (rolledData[selectedIndex]) {
             let selectedYear = rolledData[selectedIndex][0];
@@ -27,7 +27,7 @@ function filterAndRender() {
         }
     }
 
-    renderProjects(filtered, projectsContainer, 'h2'); [cite: 1191]
+    renderProjects(filtered, projectsContainer, 'h2');
     
     const titleElement = document.querySelector('.projects-title');
     if (titleElement) {
@@ -36,50 +36,50 @@ function filterAndRender() {
 }
 
 function renderPieChart(projectsGiven) {
-    let newSVG = d3.select('svg'); [cite: 1195]
-    newSVG.selectAll('path').remove(); [cite: 1196]
+    let newSVG = d3.select('svg');
+    newSVG.selectAll('path').remove();
     
-    let legend = d3.select('.legend'); [cite: 891]
+    let legend = d3.select('.legend');
     legend.selectAll('li').remove(); 
 
-    let newRolledData = d3.rollups( [cite: 1166]
+    let newRolledData = d3.rollups(
         projectsGiven,
         (v) => v.length,
         (d) => d.year
     );
 
-    let newData = newRolledData.map(([year, count]) => { [cite: 1173]
+    let newData = newRolledData.map(([year, count]) => {
         return { value: count, label: year };
     });
 
-    let newSliceGenerator = d3.pie().value((d) => d.value); [cite: 866]
+    let newSliceGenerator = d3.pie().value((d) => d.value);
     let newArcData = newSliceGenerator(newData);
-    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50); [cite: 668]
-    let newArcs = newArcData.map((d) => arcGenerator(d)); [cite: 709]
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let newArcs = newArcData.map((d) => arcGenerator(d));
 
-    newArcs.forEach((arc, i) => { [cite: 1303]
+    newArcs.forEach((arc, i) => {
         newSVG
             .append('path')
             .attr('d', arc)
             .attr('fill', colors(i))
-            .attr('class', i === selectedIndex ? 'selected' : '') [cite: 1340]
-            .on('click', () => { [cite: 1310]
-                selectedIndex = selectedIndex === i ? -1 : i; [cite: 1337]
+            .attr('class', i === selectedIndex ? 'selected' : '')
+            .on('click', () => {
+                selectedIndex = selectedIndex === i ? -1 : i;
                 
                 newSVG.selectAll('path')
-                    .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : '')); [cite: 1339]
+                    .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
                 legend.selectAll('li')
-                    .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : '')); [cite: 1347]
+                    .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
                 
                 filterAndRender(); 
             });
     });
 
-    newData.forEach((d, i) => { [cite: 890]
+    newData.forEach((d, i) => {
         legend.append('li')
-            .attr('style', `--color:${colors(i)}`) [cite: 894]
-            .attr('class', i === selectedIndex ? 'selected' : '') [cite: 1349]
-            .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`) [cite: 895]
+            .attr('style', `--color:${colors(i)}`)
+            .attr('class', i === selectedIndex ? 'selected' : '')
+            .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
             .on('click', () => {
                 selectedIndex = selectedIndex === i ? -1 : i;
                 newSVG.selectAll('path')
@@ -91,12 +91,12 @@ function renderPieChart(projectsGiven) {
     });
 }
 
-searchInput.addEventListener('input', (event) => { [cite: 1133]
-    query = event.target.value; [cite: 1135]
+searchInput.addEventListener('input', (event) => {
+    query = event.target.value;
     selectedIndex = -1; 
     filterAndRender();
 });
 
 if (projectsContainer) {
-    filterAndRender(); [cite: 1183]
+    filterAndRender();
 }
